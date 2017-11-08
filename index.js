47ec0cd56afad3743162552730e4e183790ef1ad
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-08 16:19:36 +0000

    Remove unneeded files

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.set('port', (process.env.PORT || 3000));
-app.get('/', (req, res) => res.send("Hello World!!"));
-
-app.listen(app.get('port'), () => console.log("Server listening on port", app.get('port')));
