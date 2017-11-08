d1d21d4538ffb19cff58cf929b8f0a83b890e39a
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-08 12:45:31 +0000

    Added notify

---
+++ notify.js
@@ -0,0 +1,14 @@
+const Botkit = require('botkit');
+
+const controller = Botkit.slackbot({});
+const bot = controller.spawn();
+
+bot.configureIncomingWebhook({ url: process.env.WEBHOOK_URL});
+
+const msg = 'Hello from heroku';
+
+bot.sendWebhook(msg, (err, res) => {
+  if (err) console.log(err.message);
+
+  console.log("Message delivered");
+});
