1906dfbef4deacb6c3ddebdc5cdaf98c399e8f23
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-08 14:16:43 +0000

    Refine messages

--- trend.js
+++ trend.js
@@ -1,5 +1,6 @@
 const Trending = require('github-trend');
 const Botkit = require('botkit');
+const _ = require('lodash');
 
 const controller = Botkit.slackbot({});
 const bot = controller.spawn();
@@ -15,14 +16,15 @@ let msgDefaults = {
 
 scraper.scrapeTrendingReposFullInfo('javascript')
 .then(repos => {
-    repoDetails = repos.slice(0,5).map(repo => ({
+    repoDetails = repos.slice(0, 10).map(repo => ({
       title: `${repo.owner}/${repo.name}`,
       title_link: `https://github.com/${repo.owner}/${repo.name}`,
-      text: `${repo.description}\n${repo.language} - ${repo.todaysStars}`,
+      text: `${repo.description}\nTodays stars: ${repo.todaysStars}`,
       mrkdwn_in: ['text', 'pretext'],
     }));
 
-    let msg = { attachments: repoDetails, ...msgDefaults };
+    let msg = _.defaults({ attachments: repoDetails }, msgDefaults );
+
     bot.sendWebhook(msg, (err, resp) => {
       if (err) console.log(err.message);
     })
