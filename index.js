ab6c947b69ffb3228e21f9db001f27589992bb57
Mike Quinn <mike.quinn@scotland.pnn.police.uk>
2017-11-08 11:17:41 +0000

    fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+app.set('port', (process.env.PORT || 3000));
 app.get('/', (req, res) => res.send("Hello World!!"));
 
-app.listen(3000, () => console.log("Server listening on port 3000"));
+app.listen(3000, () => console.log("Server listening on port", app.get('port')));
